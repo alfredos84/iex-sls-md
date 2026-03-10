f868ea269b7af13a5f730aec3b3a5029303faf22
Alfredo Daniel Sanchez <45202555+alfredos84@users.noreply.github.com>
2026-03-10 10:31:14 +0100

    Add files via upload

--- src/WP1/run_WP1.sh
+++ src/WP1/run_WP1.sh
@@ -1,22 +1,19 @@
 #!/bin/bash
-# run_parallel_mpi_24cores.sh
 
 date
 
 
 ################################################################################
-###--- PREPARE THE AS-MELTED GLASS FOR DIFFERENT Na/K RATIOS ---###
-
+###--- PREPARE THE AS-MELTED GLASS FOR DIFFERENT Na+/K+ RATIOS ---###
 
 cd MELTQUENCH
 
-t_final=(300)
-
-
 # --- Define x array and corresponding experimental densities for melt-quench simulations
 # --- x_values and dens_values are parsed from the file "MeltQuench.in"
-x_values=(0 1 2 3 4 5 6 9 12 15)
+
+x_values=(0 1 2 3 4 5 6 9 12 15) # Na/K ratios for melt-quench simulations
 dens_values=(2.48211 2.46275 2.46118 2.481 2.45889 2.46098 2.48284 2.47479 2.47525 2.45893)
+t_final=(300) # Final temperature for melt-quench simulations in K
 
 for ((i=0; i<${#x_values[@]}; i++)); do
   x=${x_values[i]}
@@ -26,8 +23,9 @@ for ((i=0; i<${#x_values[@]}; i++)); do
   echo "  - MeltQuench.in simulation: x=${x}, density=${dens} g/cm³, t_final=${t_final}"
 done
 
+
 ################################################################################
-###--- INTERATOMIC POTENTIAL ---###
+###--- MEAN SQUARE DISPLACEMENT CALCULATIONS ---###
 
 cd ../MSD
 
@@ -40,7 +38,7 @@ done
 
 
 ################################################################################
-###--- INTERATOMIC POTENTIAL ---###
+###--- ELASTIC PROPERTIES CALCULATIONS ---###
 
 cd ../YOUNG_MODULUS_STRAIN_STRESS
 
