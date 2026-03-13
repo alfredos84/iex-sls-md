5dce44a15cc702b08dbba9ddba189e0e16870a9b
Alfredo Daniel Sanchez <45202555+alfredos84@users.noreply.github.com>
2026-03-13 16:11:45 +0100

    Add files via upload

--- src/WP1/run_WP1.sh
+++ src/WP1/run_WP1.sh
@@ -15,13 +15,13 @@ x_values=(0 1 2 3 4 5 6 9 12 15) # Na/K ratios for melt-quench simulations
 dens_values=(2.48211 2.46275 2.46118 2.481 2.45889 2.46098 2.48284 2.47479 2.47525 2.45893)
 t_final=(300) # Final temperature for melt-quench simulations in K
 
-for ((i=0; i<${#x_values[@]}; i++)); do
-  x=${x_values[i]}
-  dens=${dens_values[i]}
+# for ((i=0; i<${#x_values[@]}; i++)); do
+#   x=${x_values[i]}
+#   dens=${dens_values[i]}
 
-  mpirun -np 48 lmp -in MeltQuench.in -var target_density ${dens} -var val_x ${x} -var T_end ${t_final}
-  echo "  - MeltQuench.in simulation: x=${x}, density=${dens} g/cm³, t_final=${t_final}"
-done
+#   mpirun -np 48 lmp -in MeltQuench.in -var target_density ${dens} -var val_x ${x} -var T_end ${t_final}
+#   echo "  - MeltQuench.in simulation: x=${x}, density=${dens} g/cm³, t_final=${t_final}"
+# done
 
 
 ################################################################################
@@ -31,11 +31,11 @@ cd ../YOUNG_MODULUS_STRAIN_STRESS
 
 for ((i=0; i<${#x_values[@]}; i++)); do
   x=${x_values[i]}
-  mpirun -np 48 lmp -in input_lammps_x.in -var val_x ${x} -var T_end ${t_final}
+  mpirun -np 4 lmp -in input_lammps_x.in -var val_x ${x} -var T_end ${t_final}
   echo "  - input_lammps_x.in simulation: x=${x}, t_final=${t_final}"
-  mpirun -np 48 lmp -in input_lammps_y.in -var val_x ${x} -var T_end ${t_final}
+  mpirun -np 4 lmp -in input_lammps_y.in -var val_x ${x} -var T_end ${t_final}
   echo "  - input_lammps_y.in simulation: x=${x}, t_final=${t_final}"
-  mpirun -np 48 lmp -in input_lammps_z.in -var val_x ${x} -var T_end ${t_final}
+  mpirun -np 4 lmp -in input_lammps_z.in -var val_x ${x} -var T_end ${t_final}
   echo "  - input_lammps_z.in simulation: x=${x}, t_final=${t_final}"
 done
 
